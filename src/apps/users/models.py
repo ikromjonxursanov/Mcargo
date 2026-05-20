b66a8a55236d04517ce15743dfb93562af421a53
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-05-20 20:01:24 +0500

    feat(user) models

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,21 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+phone_validator = RegexValidator(regex=r'^\+9989\d{9}$',
+                             massage='telefon number soralgan fromatda bolsihi kerka ')
 
 class User(AbstractUser):
-    ROLLE_CHOICES= [
-        ('drivers', 'drivers'),
-        ('manager', 'manager'),
-        ('consignor', 'consignor'),
+    ROLE_CHOICES= [
+        ('driver', 'Driver'),
+        ('manager', 'Manager'),
+        ('consignor', 'Consignor'),
     ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone_number = models.CharField(max_length=9, validators=[phone_validator])
 
+    def __str__(self):
+        return self.role
 
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     full_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=13)
+    phone_number = models.CharField(max_length=9, unique=True, validators=[phone_validator])
     score = models.FloatField(default=0)
     cenceled_offer = models.BooleanField(default=False)
     learned = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=9, unique=True, validators=[phone_validator])
+    code = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.full_name
