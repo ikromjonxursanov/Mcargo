459123e59a8170698ae48dd4763704f0adba310b
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-05-20 20:03:18 +0500

    reafctor(user) admin

--- src/apps/users/admin.py
+++ src/apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Driver
+from .models import User, Driver, Manager
 
 admin.site.register(User)
 admin.site.register(Driver)
+admin.site.register(Manager)
 
