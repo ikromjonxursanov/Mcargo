0b57c9c27895e01938ac688e38db0758e2638cb5
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-05-21 12:02:02 +0500

    refactor(apps):loads:models

--- src/apps/loads/models.py
+++ src/apps/loads/models.py
@@ -31,7 +31,7 @@ class Driverlocation(models.Model):
 
 
     driver = models.OneToOneField(Driver, on_delete=models.CASCADE, related_name="driverlocation")
-    
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="empty")
 
     longitude = models.DecimalField(default=0, max_digits=10, decimal_places=6)
     latitude = models.DecimalField(default=0, max_digits=10, decimal_places=6)
